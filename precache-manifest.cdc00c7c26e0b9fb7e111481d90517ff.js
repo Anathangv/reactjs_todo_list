c6b23076a8f40d86f576b16cbbceded6a5b06451
Anathangv <anathan.gv@gmail.com>
2020-07-04 14:57:57 -0300

    Updates

---
+++ precache-manifest.cdc00c7c26e0b9fb7e111481d90517ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "869de30472fc9fdc0f1afceb1b2f8582",
+    "url": "/reactjs_todo_list/index.html"
+  },
+  {
+    "revision": "ff7bf6e8350fa52e3efe",
+    "url": "/reactjs_todo_list/static/css/main.da5c2a6e.chunk.css"
+  },
+  {
+    "revision": "65930f5b7a6d6183266b",
+    "url": "/reactjs_todo_list/static/js/2.86b4c790.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs_todo_list/static/js/2.86b4c790.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff7bf6e8350fa52e3efe",
+    "url": "/reactjs_todo_list/static/js/main.04846bfb.chunk.js"
+  },
+  {
+    "revision": "6b491186de39fc6f4e65",
+    "url": "/reactjs_todo_list/static/js/runtime-main.15332fe0.js"
+  }
+]);
